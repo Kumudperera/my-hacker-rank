1a5aca2dc274c51c2be901d4ba4e54c208a230f5
kumud.perera <nethmalperera54@gmail.com>
2026-05-04 21:23:29 +0530

    easy.gradingStudents completed

---
+++ problem-solving/by-python/easy/gradingStudents/main.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    result = []
+    for g in grades:
+        if g < 38:
+            result.append(g)
+            continue
+
+        rem = g % 5
+        diff = 5 - rem if rem != 0 else 0
+
+        if 0 < diff < 3:
+            result.append(g + diff)
+        else:
+            result.append(g)
+
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
